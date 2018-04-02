97473b21d5d43919f0d161d09995d02e185a6e44
Mirza Sisic <mirzasisic57@gmail.com>
2018-04-02 14:28:09 -0400

    Lesson3

--- scripts/lessons.js
+++ scripts/lessons.js
@@ -7,28 +7,36 @@ $(document).ready(function() {
   $('#btn1').css({opacity:'0.5'});
 
   //Lesson Two
-  $('#btn2').html('<strong>Bold</strong>');
-  $('#btn1').on('click', function() {
-    $('#panel1').fadeToggle();
-  });
-  $('#panel1').on('mouseover', function() {
-    $('#panel1').fadeToggle();
-  });
-  $('#btn2').on('click', function() {
-    $('#panel2').fadeToggle();
-  });
-  $('#btn3').on('click', function() {
-    $('#panel3').fadeToggle();
-  });
-  $('#btn4').on('click', function() {
-    $('#panel4').fadeToggle();
-  });
-  $('#panel4').on('click', function() {
-    $('#panel4 .panel-body').html('New content');
-  });
+  // $('#btn2').html('<strong>Bold</strong>');
+  // $('#btn1').on('click', function() {
+  //   $('#panel1').fadeToggle();
+  // });
+  // $('#panel1').on('mouseover', function() {
+  //   $('#panel1').fadeToggle();
+  // });
+  // $('#btn2').on('click', function() {
+  //   $('#panel2').fadeToggle();
+  // });
+  // $('#btn3').on('click', function() {
+  //   $('#panel3').fadeToggle();
+  // });
+  // $('#btn4').on('click', function() {
+  //   $('#panel4').fadeToggle();
+  // });
+  // $('#panel4').on('click', function() {
+  //   $('#panel4 .panel-body').html('New content');
+  // });
 
   //Lesson three
+  var content = "My new content";
+
+  $('.panel-button').on('click',  function() {
+      var panel = $(this).attr('data-panel');
+      $('#'+panel).toggle();
+      $('#'+panel+'.panel-body').html(content);
+  });
   
+  //Lesson Four
  
 
 
