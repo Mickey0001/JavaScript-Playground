f4e7382e3ca09e4c56c59c718f1d8ad1abd8202c
Mirza Sisic <mirzasisic57@gmail.com>
2018-04-05 16:34:25 -0400

    POST and GET

--- scripts/ajax.js
+++ scripts/ajax.js
@@ -1,14 +1,40 @@
 $(function () {
 
 var $orders = $('#orders');
+var $name = $('#name');
+var $drink = $('#drink');
+
+function addOrder(order){
+  $orders.append('<li class="list-group-item"><strong>Name: </strong>' + order.name + ' ' + '<strong>Drink: </strong>' + order.drink + '</li>');
+}
 
   $.ajax({
     type: 'GET',
-    url: 'http://rest.learncode.academy/api/Mirza/5ab288d22bfd57010088a9b8',
+    url: 'http://rest.learncode.academy/api/:user/:collection',
     success: function(orders) {
       $.each(orders, function(i, order) {
-        $orders.append('<li>Name:' + order.name + ', Drink' + order.drink + '</li>');
+       addOrder(order);
       });
+    },
+    error: function() {
+      alert('error loading order');
     }
   });
+  $('#add-order').on('click', function() {
+    var order = {
+      name: $name.val(),
+      drink: $drink.val(),
+    };
+    $.ajax({
+      type: 'POST',
+      url: 'http://rest.learncode.academy/api/orders/mirza',
+      data: order,
+      success: function(newOrder) {
+        addOrder(newOrder);
+      },
+      error: function() {
+        alert('error posting order');
+      }
+    });
+  })
 });
