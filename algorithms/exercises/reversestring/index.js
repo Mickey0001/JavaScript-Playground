9cd2a449981c1a2db8be459f363292828f5b5805
Mirza Sisic <mirzasisic57@gmail.com>
2018-10-22 17:53:05 +0200

    Reverse a String in 3 ways

--- algorithms/exercises/reversestring/index.js
+++ algorithms/exercises/reversestring/index.js
@@ -7,7 +7,48 @@
 //   reverse('Greetings!') === '!sgniteerG'
 
 function reverse(str) {
- return (str === '') ? '' : reverse(str.substr(1)) + str.charAt(0);
+
+//Reverse with ternary operator
+/*
+return (str === '') ? '' : reverse(str.substr(1)) + str.charAt(0);
+*/
+
+
+//Turn to array and reverse
+/*
+const array = str.split('');
+array.reverse();
+return array.join('');
+*/
+
+
+//Reverse helper
+/*
+return str
+  .split('')
+  .reverse()
+  .join('');
+  */
+
+
+//Loop
+/* 
+let reversed = '';
+
+for (let character of str)
+
+
+{
+  reversed = character + reversed;
+}
+
+return reversed;
+*/
+
+
+//Reduce method
+  return str.split('').reduce((reversed, character) => character + reversed, '');
+
 }
 
 module.exports = reverse;
