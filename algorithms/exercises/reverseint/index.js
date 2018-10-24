0cbed8af3fb284b8582c0051749589f226e87ad1
Mirza Sisic <mirzasisic57@gmail.com>
2018-10-24 22:32:21 +0200

    Reverse Integer 4/4

--- algorithms/exercises/reverseint/index.js
+++ algorithms/exercises/reverseint/index.js
@@ -12,6 +12,11 @@ function reverseInt(n)
 {
  const reversed = n.toString().split('').reverse().join('');
 
+ if (n < 0) 
+ {
+  return parseInt(reversed) * -1;
+ }
+
  return parseInt(reversed);
 }
 
