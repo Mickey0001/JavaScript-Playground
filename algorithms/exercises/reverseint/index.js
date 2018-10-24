4633ed1850f492d98e0a6a362af7de4b3b2c7ab6
Mirza Sisic <mirzasisic57@gmail.com>
2018-10-24 22:28:49 +0200

    Reverse Integer

--- algorithms/exercises/reverseint/index.js
+++ algorithms/exercises/reverseint/index.js
@@ -8,6 +8,9 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) 
+{
+  n.toString().split.reverse('')
+}
 
 module.exports = reverseInt;
