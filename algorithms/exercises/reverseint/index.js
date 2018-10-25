4b0f3f36dec90cf87f5eb699ce1862ae0c00ff33
Mirza Sisic <mirzasisic57@gmail.com>
2018-10-25 20:58:58 +0200

    Reverse Int

--- algorithms/exercises/reverseint/index.js
+++ algorithms/exercises/reverseint/index.js
@@ -12,12 +12,7 @@ function reverseInt(n)
 {
  const reversed = n.toString().split('').reverse().join('');
 
- if (n < 0) 
- {
-  return parseInt(reversed) * -1;
- }
-
- return parseInt(reversed);
+ return parseInt(reversed) * Math.sign(n);
 }
 
 module.exports = reverseInt;
