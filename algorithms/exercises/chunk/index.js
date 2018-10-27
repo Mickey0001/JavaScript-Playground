f6da6dac394ba7ca61e1ab57cbaaeaed81663696
Mirza Sisic <mirzasisic57@gmail.com>
2018-10-27 03:57:25 +0200

    Array Chunks - First Attempt

--- algorithms/exercises/chunk/index.js
+++ algorithms/exercises/chunk/index.js
@@ -8,6 +8,13 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) 
+{
+let r = [];
+
+for (let i = 0, len = array.length; i < length; i+= size)
+  r.push(array.slice(i, i + size));
+return r;
+}
 
 module.exports = chunk;
