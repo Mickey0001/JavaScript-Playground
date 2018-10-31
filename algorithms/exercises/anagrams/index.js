4fa2d2dd5ffa83e8a89275ee096b6ac6c8d1a7e6
Mirza Sisic <mirzasisic57@gmail.com>
2018-10-31 18:01:53 +0100

    Anagrams - Second Shorter Solution

--- algorithms/exercises/anagrams/index.js
+++ algorithms/exercises/anagrams/index.js
@@ -10,33 +10,12 @@
 
 function anagrams(stringA, stringB) 
 {
-  const aCharMap = buildCharMap(stringA);
-  const bCharMap = buildCharMap(stringB);
-
-  if(Object.keys(aCharMap).length !== Object.keys(bCharMap).length)
-  {
-    return false;
-  }
-
-  for (let char in aCharMap)
-  {
-    if (aCharMap[char] !== bCharMap[char])
-    {
-      return false;
-    }
-  }
-  return true;
+  return cleanString(stringA) === cleanString(stringB);
 }
 
-function buildCharMap(str)
+function cleanString(str)
 {
-  const charMap = {};
-
-  for (let char of str.replace(/[^\w]/g, '').toLowerCase())
-  {
-    charMap[char] = charMap[char] + 1 || 1;
-  }
-  return charMap;
+  return str.replace(/[^\w]/g, '').toLowerCase().split('').sort().join('');
 }
 
 module.exports = anagrams;
