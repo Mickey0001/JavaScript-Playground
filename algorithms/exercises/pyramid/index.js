def3715803ac7149e4102975dee06da3d20022bf
Mirza Sisic <mirzasisic57@gmail.com>
2018-11-02 21:11:51 +0100

    Pyramid - Second Solution

--- algorithms/exercises/pyramid/index.js
+++ algorithms/exercises/pyramid/index.js
@@ -14,27 +14,31 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) 
+function pyramid(n, row = 0, level = '') 
 {
-  const midpoint = Math.floor((2 * n - 1) / 2);
-
-  for (let row = 0; row < n; row ++)
+  if (row === n)
   {
-    let level = '';
-
-    for (let column = 0; column < 2 * n - 1; column ++)
-    {
-      if (midpoint - row <= column && midpoint + row >= column)
-      {
-        level += '#';
-      }
-      else
-      {
-        level += ' ';
-      }
-    }
-    console.log(level);
+    return;
   }
+
+  if (level.length === 2 * n - 1)
+  {
+    console.log(level);
+    return pyramid(n, row + 1);
+  }
+
+  const midpoint = Math.floor((2 * n - 1) / 2);
+  let add;
+
+  if (midpoint - row <= level.length && midpoint + row >= level.length)
+  {
+    add = '#';
+  }
+  else
+  {
+    add = ' ';
+  }
+  pyramid(n, row, level + add);
 }
 
 module.exports = pyramid;
