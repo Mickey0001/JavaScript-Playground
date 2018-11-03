5a9cf6e5bf255784cb3d212186591f58308a57ef
Mirza Sisic <mirzasisic57@gmail.com>
2018-11-03 04:07:07 +0100

    Vowels - First Solution

--- algorithms/exercises/vowels/index.js
+++ algorithms/exercises/vowels/index.js
@@ -1,12 +1,25 @@
 // --- Directions
 // Write a function that returns the number of vowels
-// used in a string.  Vowels are the characters 'a', 'e'
+// used in a string.  Vowels are the stracters 'a', 'e'
 // 'i', 'o', and 'u'.
 // --- Examples
 //   vowels('Hi There!') --> 3
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels(str) 
+{
+  let counter = 0;
+  const checker = ['a', 'e', 'i', 'o', 'u'];
+
+  for (let chararcter of str.toLowerCase())
+  {
+    if(checker.includes(chararcter))
+    {
+      counter ++;
+    }
+  }
+return counter;
+}
 
 module.exports = vowels;
