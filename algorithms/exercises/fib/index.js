e3530eb16c6017479264074477dc03f858e16fb1
Mirza Sisic <mirzasisic57@gmail.com>
2018-11-05 21:23:04 +0100

    Fib - Second Short Solution

--- algorithms/exercises/fib/index.js
+++ algorithms/exercises/fib/index.js
@@ -10,16 +10,28 @@
 
 function fib(n) 
 {
-  const result = [0,1];
-
-  for (let i = 2; i <= n; i ++)
+  if (n < 2)
   {
-    const a = result[i - 1];
-    const b = result[i - 2];
-
-    result.push(a + b);
+    return n;
   }
-  return result[n];
+
+  return fib(n - 1) + fib(n - 2);
 }
+
+// //Iterative Soltuion
+// function fib(n) 
+// {
+  
+//   const result = [0,1];
+
+//   for (let i = 2; i <= n; i ++)
+//   {
+//     const a = result[i - 1];
+//     const b = result[i - 2];
+
+//     result.push(a + b);
+//   }
+//   return result[n];
+// }
 
 module.exports = fib;
