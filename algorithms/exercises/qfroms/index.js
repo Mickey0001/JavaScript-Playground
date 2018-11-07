98aacc5fc5f33d61c068d1142b5620ce4cf01944
Mirza Sisic <mirzasisic57@gmail.com>
2018-11-07 22:06:54 +0100

    Queues and Stacks

--- algorithms/exercises/qfroms/index.js
+++ algorithms/exercises/qfroms/index.js
@@ -16,7 +16,48 @@ const Stack = require('./stack');
 
 class Queue 
 {
-  
+  constructor()
+  {
+    this.firstStack = new Stack();
+    this.secondStack = new Stack();
+  }
+
+  add(record)
+  {
+    this.firstStack.push(record);
+  }
+
+  remove()
+  {
+    while (this.firstStack.peek())
+    {
+      this.secondStack.push(this.firstStack.pop());
+    }
+
+    const record = this.secondStack.pop();
+
+    while (this.secondStack.peek())
+    {
+      this.firstStack.push(this.secondStack.pop());
+    }
+    return record;
+  }
+
+  peek()
+  {
+    while (this.firstStack.peek())
+    {
+      this.secondStack.push(this.firstStack.pop());
+    }
+
+    const record = this.secondStack.peek();
+
+    while (this.secondStack.peek())
+    {
+      this.firstStack.push(this.secondStack.pop());
+    }
+    return record;
+  }
 }
 
 module.exports = Queue;
