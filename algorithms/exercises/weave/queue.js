924aef4d241528b18989231f738788f867a90c64
Mirza Sisic <mirzasisic57@gmail.com>
2018-11-07 17:38:30 +0100

    Weave - Solution

--- algorithms/exercises/weave/queue.js
+++ algorithms/exercises/weave/queue.js
@@ -4,17 +4,26 @@
 // one to be returned) from the queue *without*
 // removing it.
 
-class Queue {
-  constructor() {
+class Queue 
+{
+  constructor() 
+  {
     this.data = [];
   }
 
-  add(record) {
+  add(record) 
+  {
     this.data.unshift(record);
+  } 
+
+  remove() 
+  {
+    return this.data.pop();
   }
 
-  remove() {
-    return this.data.pop();
+  peek()
+  {
+    return this.data[this.data.length -1];
   }
 }
 
