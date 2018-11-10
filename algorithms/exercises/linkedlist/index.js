b537e4cacb0f6933aa71d6b9b0aeae7bf7740850
Mirza Sisic <mirzasisic57@gmail.com>
2018-11-10 17:25:27 +0100

    LinkedLists

--- algorithms/exercises/linkedlist/index.js
+++ algorithms/exercises/linkedlist/index.js
@@ -17,6 +17,99 @@ class LinkedList
   {
     this.head = null;
   }
+
+  insertFirst(data)
+  {
+    this.head = new Node(data, this.head)
+  }
+
+  size()
+  {
+    let counter = 0;
+    let node = this.head;
+
+    while (node)
+    {
+      counter++;
+      node = node.next;
+    }
+    return counter;
+  }
+
+  getFirst()
+  {
+    return this.head;
+  }
+
+  getLast()
+  {
+    if (!this.head)
+    {
+      return null;
+    }
+
+    let node = this.head;
+    while (node)
+    {
+      if (!node.next)
+      {
+        return node;
+      }
+      node = node.next;
+    }
+  }
+
+  clear()
+  {
+    this.head = null;
+  }
+
+  removeFirst()
+  {
+    if (!this.head)
+    {
+      return;
+    }
+    this.head = this.head.next;
+  }
+
+  removeLast()
+  {
+    if (!this.head)
+    {
+      return;
+    }
+
+    if (!this.head.next)
+    {
+      this.head = null;
+      return;
+    }
+
+    let previous = this.head;
+    let node = this.head.next;
+
+    while(node.next)
+    {
+      previous = node;
+      node = node.next;
+    }
+    previous.next = null;
+  }
+
+    insertLast(data) 
+    {
+      const last = this.getLast();
+
+      if (last) 
+    {
+        last.next = new Node(data);
+      } 
+     else 
+     {
+        this.head = new Node(data);
+      }
+    }
 }
 
 module.exports = { Node, LinkedList };
