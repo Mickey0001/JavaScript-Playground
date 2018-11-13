8668215b92f79d1fe8bda8b10977cf4d7b9f3dde
Mirza Sisic <mirzasisic57@gmail.com>
2018-11-13 04:55:18 +0100

    Generators...

--- algorithms/exercises/linkedlist/index.js
+++ algorithms/exercises/linkedlist/index.js
@@ -169,6 +169,28 @@ class LinkedList
       const node = new Node(data, previous.next);
       previous.next = node;
     }
+
+    forEach(fn)
+    {
+      let node = this.head;
+      let counter = 0;
+      while (node)
+      {
+        fn(node, counter);
+        node = node.next;
+        counter++;
+      }
+    }
+
+    *[Symbol.iterator]()
+    {
+      let node = this.head;
+      while (node)
+      {
+        yield node;
+        node = node.next;
+      }
+    }
 }
 
 module.exports = { Node, LinkedList };
