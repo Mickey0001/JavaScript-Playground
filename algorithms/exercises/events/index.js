4521fef865c809111fa3353b55ddf129f98a973f
Mirza Sisic <mirzasisic57@gmail.com>
2018-11-19 17:03:12 +0100

    Events

--- algorithms/exercises/events/index.js
+++ algorithms/exercises/events/index.js
@@ -3,17 +3,27 @@
 // Events class.  The Events class should
 // have methods 'on', 'trigger', and 'off'.
 
-class Events {
+class Events 
+{
   // Register an event handler
-  on(eventName, callback) {}
+  on(eventName, callback) 
+  {
+
+  }
 
   // Trigger all callbacks associated
   // with a given eventName
-  trigger(eventName) {}
+  trigger(eventName) 
+  {
+
+  }
 
   // Remove all event handlers associated
   // with the given eventName
-  off(eventName) {}
+  off(eventName) 
+  {
+
+  }
 }
 
 module.exports = Events;
