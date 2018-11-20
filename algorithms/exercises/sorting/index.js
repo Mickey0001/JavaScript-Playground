d62ee5ccaca899e6512792a9e9f0d967b0b15794
Mirza Sisic <mirzasisic57@gmail.com>
2018-11-20 22:34:47 +0100

    Merge Sort

--- algorithms/exercises/sorting/index.js
+++ algorithms/exercises/sorting/index.js
@@ -1,20 +1,76 @@
 // --- Directions
 // Implement bubbleSort, selectionSort, and mergeSort
 
-function bubbleSort(arr) {
-
+function bubbleSort(arr) 
+{
+  for (let i = 0; i < arr.length; i++)
+  {
+    for (let j = 0; j < (arr.length - i - 1); j++)
+    {
+      if (arr[j] > arr[j + 1])
+      {
+        const lesser = arr[j + 1];
+        arr[j + 1] = arr[j];
+        arr[j] = lesser;
+      }
+    }
+  }
+  return arr;
 }
 
-function selectionSort(arr) {
-
+function selectionSort(arr) 
+{
+  for (let i = 0; i < arr.length; i++)
+  {
+    let indexofMin = i;
+  }
+  for (let j = i + 1; j < arr.length; j++)
+  {
+    if (arr[j] < arr[indexedOfMin])
+    {
+      indexedOfMin = j;
+    }
+    if (indexedOfMin !== i)
+    {
+      let lesser = arr[indexedOfMin];
+      arr[indexedOfMin] = arr[i];
+      arr[i] = lesser;
+    }
+  }
+  return arr;
 }
 
-function mergeSort(arr) {
+function mergeSort(arr) 
+{
+  if (arr.length === 1)
+  {
+    return arr;
+  }
 
+  const center = Math.floor(arr.length / 2);
+  const left = arr.slice(0, center);
+  const right = arr.slice(center);
+
+  return merge(mergeSort(left), mergeSort(right));
 }
 
-function merge(left, right) {
+function merge(left, right) 
+{
+  const results = [];
 
+  while (left.length && right.length)
+  {
+    if (left[0] < right[0])
+    {
+      results.push(left.shift());
+    }
+    else
+    {
+      results.push(right.shift());
+    }
+  }
+
+  return [...results, ...left, ...right];
 }
 
 module.exports = { bubbleSort, selectionSort, mergeSort };
