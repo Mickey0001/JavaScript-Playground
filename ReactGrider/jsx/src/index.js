623a9a8befcd65ec42d7d83827e4c24edb99054f
Mirza Sisic <mirzasisic57@gmail.com>
2018-11-22 22:32:50 +0100

    Start Back From Zilch

--- ReactGrider/jsx/src/index.js
+++ ReactGrider/jsx/src/index.js
@@ -1,12 +1,12 @@
+//Import React and React DOM
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//Create an component
+const App = () => 
+{
+  return <div>Waazzzuuup!</div>
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+//Render the component
+ReactDOM.render(<App/>, document.querySelector('#root'));
