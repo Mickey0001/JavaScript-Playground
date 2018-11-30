4c58f2cbced9d686b4f43e2f59f82a285a1c9f90
Mickey0001 <mirzasisic57@gmail.com>
2018-11-30 21:16:26 +0100

    New project with Faker.js

--- ReactGrider/components/src/index.js
+++ ReactGrider/components/src/index.js
@@ -1,9 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const APP = () =>
+const App = () =>
 {
-  return <div>Waazzzaap!</div>
+  return (
+    <div className="ui container comments">
+      <div className="comment">
+        <a href="/" className="avatar">
+          <img src="" alt="avatar"/>
+        </a>
+        <div className="content">
+          <a href="/" className="author">
+            Mirza
+          </a>
+          <div className="metadata">
+            <span className="date">Today at 18:00</span>
+          </div>
+          <div className="text">
+            Very nice post.
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 };
 
 ReactDOM.render(<App/>, document.querySelector('#root'));
