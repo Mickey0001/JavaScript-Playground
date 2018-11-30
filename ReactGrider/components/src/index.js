8064265e5b8b45c4d26d1bca7fd212f9fea9321c
Mickey0001 <mirzasisic57@gmail.com>
2018-11-30 21:05:22 +0100

    Componenets Project

---
+++ ReactGrider/components/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const APP = () =>
+{
+  return <div>Waazzzaap!</div>
+};
+
+ReactDOM.render(<App/>, document.querySelector('#root'));
