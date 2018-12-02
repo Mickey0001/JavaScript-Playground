9ee5eadd0550e5277fe7f6ca206b8a53fe5e9203
Mickey0001 <mirzasisic57@gmail.com>
2018-12-02 20:57:58 +0100

    Re-using components

--- ReactGrider/components/src/index.js
+++ ReactGrider/components/src/index.js
@@ -2,14 +2,39 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import faker from 'faker';
 import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
+import './App.css';
 
 const App = () =>
 {
   return (
     <div className="ui container comments">
-       <CommentDetail avatar={faker.image.avatar()} time="Today at: 4:50 PM" text="Okay"  author="Munir"/>
-       <CommentDetail avatar={faker.image.avatar()} time="Today at: 2:33 PM" text="Cool" author="Haso"/>
-       <CommentDetail avatar={faker.image.avatar()} time="Today at: 1:26 PM" text="Nice" author="Ekrem"/>
+       <ApprovalCard>
+          <CommentDetail 
+            avatar={faker.image.avatar()} 
+            time="Today at: 4:50 PM" 
+            text="There is no Burek with cheese!"  
+            author="Munir"
+          />
+      </ApprovalCard>
+
+        <ApprovalCard>
+          <CommentDetail 
+            avatar={faker.image.avatar()} 
+            time="Today at: 2:33 PM" 
+            text="Foča is better then Sarajevo..." 
+            author="Haso"
+          />
+       </ApprovalCard>
+
+       <ApprovalCard>
+          <CommentDetail 
+            avatar={faker.image.avatar()} 
+            time="Today at: 1:26 PM" 
+            text="El-Bake is the king of Kekistan." 
+            author="Ekrem"
+          />
+       </ApprovalCard>
     </div>
   )
 };
