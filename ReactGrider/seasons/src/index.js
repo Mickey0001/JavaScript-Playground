1df0fb30da897ebff7f2849fd452fa6d83c17f6a
Mickey0001 <mirzasisic57@gmail.com>
2018-12-04 20:14:49 +0100

    Working with State

--- ReactGrider/seasons/src/index.js
+++ ReactGrider/seasons/src/index.js
@@ -3,6 +3,13 @@ import ReactDOM from 'react-dom';
 
 class App extends React.Component 
 {
+  constructor(props)
+  {
+    super(props);
+
+    this.state = { lat: null };
+  }
+
   render()
   {
     window.navigator.geolocation.getCurrentPosition(
