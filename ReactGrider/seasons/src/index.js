f6d4ce4d7f9ae447710f7e41e8ca84a58eb69f41
Mickey0001 <mirzasisic57@gmail.com>
2018-12-04 21:46:57 +0100

    Working with state

--- ReactGrider/seasons/src/index.js
+++ ReactGrider/seasons/src/index.js
@@ -7,18 +7,36 @@ class App extends React.Component
   {
     super(props);
 
-    this.state = { lat: null };
+    //this is the only time we do direct assignment to this.state
+    this.state = { lat: null, errorMessage: '' };
+
+    window.navigator.geolocation.getCurrentPosition(
+      position => 
+        {
+          this.setState({ lat: position.coords.latitude });
+        },
+      err => 
+        {
+          this.setState({ errorMessage: err.message })
+        }
+    );
   }
 
   render()
   {
-    window.navigator.geolocation.getCurrentPosition(
-      (position) => console.log(position),
-      (err) => console.log(err)
-    );
-
-    return <div>Latitude: </div>;
+    if (this.state.errorMessage && !this.state.lat)
+      {
+        return <div>Error: {this.state.errorMessage} </div>
+      }
+    
+    if (!this.state.errorMessage && this.state.lat)
+      {
+        return <div>Latitude: {this.state.lat} </div>
+      }
+    
+    return <div>Loading...</div>
   }
+
 }
 
 ReactDOM.render(<App/>, document.querySelector('#root'));
