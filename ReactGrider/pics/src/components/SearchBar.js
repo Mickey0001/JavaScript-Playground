d28ad31e42b38a9b4719fa3b10f6fe668d2a9139
Mickey0001 <mirzasisic57@gmail.com>
2018-12-16 19:15:34 +0100

    Refactoring the Search Component

--- ReactGrider/pics/src/components/SearchBar.js
+++ ReactGrider/pics/src/components/SearchBar.js
@@ -2,10 +2,7 @@ import React from 'react';
 
 class SearchBar extends React.Component 
 {
-  onInputChange(event)
-  {
-    console.log(event.target.value);
-  }
+ state = { term: '' };
 
   render()
   {
@@ -14,7 +11,11 @@ class SearchBar extends React.Component
         <form className="ui form">
           <div className="field">
             <label htmlFor="">Image Search</label>
-            <input type="text" onChange={this.onInputChange}/>
+            <input 
+              type="text" 
+              value={this.state.term} 
+              onChange={(e) => this.setState({ term: e.target.value })}
+            />
           </div>
         </form>
       </div>
