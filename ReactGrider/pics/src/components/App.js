22bb97245fbfab3781272c81f2e556970c006b20
Mickey0001 <mirzasisic57@gmail.com>
2018-12-17 22:56:33 +0100

    Working on the Form

--- ReactGrider/pics/src/components/App.js
+++ ReactGrider/pics/src/components/App.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import SearchBar from './SearchBar';
 
-const App = () => 
+class App extends React.Component
 {
-  return(
-    <div className="ui container" style={{marginTop:'10px'}}>
-      <SearchBar/>
-    </div>
-  ); 
+  onSearchSubmit(term)
+  {
+    console.log(term);
+  }
+
+  render()
+  {
+    return(
+      <div className="ui container" style={{marginTop:'10px'}}>
+        <SearchBar onSubmit={this.onSearchSubmit}/>
+      </div>
+    ); 
+  }
 }
 
 export default App;
--- ReactGrider/pics/src/components/SearchBar.js
+++ ReactGrider/pics/src/components/SearchBar.js
@@ -4,11 +4,18 @@ class SearchBar extends React.Component
 {
  state = { term: '' };
 
+ onFormSubmit = event =>
+ {
+   event.preventDefault();
+
+   this.props.onSubmit(this.state.term);
+ }
+
   render()
   {
     return(
       <div className="ui segment">
-        <form className="ui form">
+        <form onSubmit={this.onFormSubmit} className="ui form">
           <div className="field">
             <label htmlFor="">Image Search</label>
             <input 
