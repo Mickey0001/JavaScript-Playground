e652b15f9b2827f4b4a7af3aacb1e443c962b602
Mickey0001 <mirzasisic57@gmail.com>
2018-12-18 17:19:32 +0100

    Unsplash API & Axios

--- ReactGrider/pics/src/components/App.js
+++ ReactGrider/pics/src/components/App.js
@@ -1,11 +1,26 @@
 import React from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar';
 
 class App extends React.Component
 {
-  onSearchSubmit(term)
+ state = { images: [] };
+
+ async onSearchSubmit(term)
   {
-    console.log(term);
+    const response = await axios
+    .get('https://api.unsplash.com/search/photos', 
+    {
+      params:
+      {
+        query: term
+      },
+      headers:
+      {
+        Authorization: 'Client-ID 02861f565eb98fb2495f2402643d4ff1e5038d549fb7b8e74c4b6e0a49212035'
+      }
+    });
+    console.log(response.data.results)
   }
 
   render()
