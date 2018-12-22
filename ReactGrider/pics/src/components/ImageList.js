7acabe0ac0b47e5a3eacd8e4796217ebee8a13ab
Mickey0001 <mirzasisic57@gmail.com>
2018-12-22 05:33:32 +0100

    Work on Image Grid

--- ReactGrider/pics/src/components/ImageList.js
+++ ReactGrider/pics/src/components/ImageList.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import './ImageList.css';
+import Imagecard from './ImageCard';
 
 const ImageList = (props) => 
 {
-  const images = props.images.map(({id, urls, description}) => 
+  const images = props.images.map(image => 
   {
-    return <img key={id} src={urls.regular} alt={description}/>
+    return <Imagecard key={image.id} image={image}/>;
   });
-  return <div>{images}</div>;
+  return <div className="image-list">{images}</div>;
 }
 
 export default ImageList;
