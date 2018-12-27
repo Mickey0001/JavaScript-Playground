23deadf3fd8773e4d7fb4e514fa8da7d61230e0e
Mickey0001 <mirzasisic57@gmail.com>
2018-12-27 18:38:55 +0100

    Finishing Touches

--- ReactGrider/vids/src/apis/youtube.js
+++ ReactGrider/vids/src/apis/youtube.js
@@ -7,7 +7,7 @@ export default axios.create(
   baseURL: 'https://www.googleapis.com/youtube/v3',
   params: {
     part: 'snippet',
-    maxResults: 4,
+    maxResults: 5,
     key: KEY
   }
 });
--- ReactGrider/vids/src/components/App.js
+++ ReactGrider/vids/src/components/App.js
@@ -2,10 +2,16 @@ import React from 'react';
 import SearchBar from './SearchBar';
 import youtube from '../apis/youtube';
 import VideoList from './VideoList';
+import VideoDetail from './VideoDetail';
 
 class App extends React.Component
 {
   state = { videos: [], selectedVideo: null };
+
+  componentDidMount()
+  {
+    this.onTermSubmit('dank memes');
+  }
 
   onTermSubmit =  async term =>
   {
@@ -17,12 +23,15 @@ class App extends React.Component
       }
     });
     
-    this.setState({ videos: response.data.items });
+    this.setState({ 
+      videos: response.data.items,
+      selectedVideo: response.data.items[0] 
+    });
   };
 
   onVideoSelect = (video) => 
   {
-    console.log('from the app', video);
+    this.setState({ selectedVideo: video });
   }
 
   render()
@@ -30,7 +39,16 @@ class App extends React.Component
     return(
       <div className="ui container">
         <SearchBar onFormSubmit={this.onTermSubmit} />
-        <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos} />
+       <div className="ui grid">
+        <div className="ui row">
+          <div className="eleven wide column">
+            <VideoDetail video={this.state.selectedVideo} />
+          </div>
+          <div className="five wide column">
+            <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos} />
+          </div>
+        </div>
+       </div>
       </div>
     );
   }
