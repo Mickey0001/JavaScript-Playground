3b0880dfb3c237cde6b022071d0edccffdce8a1d
Mickey0001 <mirzasisic57@gmail.com>
2019-01-09 20:10:35 +0100

    Persons Component

---
+++ React/reac-complete-guide/src/components/Persons/Persons.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Person from './Person/Person'
+
+const persons = (props) => (
+  persons.map((person, index) => 
+  {
+    return <Person 
+    name={person.name} 
+    click={() => props.clicked(index)}
+    age={person.age}
+    key={person.id}
+    changed={(event) => props.changed(event, person)}
+    />
+  })
+);
